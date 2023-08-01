3517932218c137439e428dc6c72d6361f0ec2347
thedarkarchitect <romanreynolds240@gmail.com>
2023-08-01 10:15:07 +0300

    first commit

---
+++ app/src/main/java/com/example/marsphotos/ui/screens/HomeScreen.kt
@@ -0,0 +1,39 @@
+package com.example.marsphotos.ui.screens
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.marsphotos.R
+import com.example.marsphotos.ui.theme.MarsPhotosTheme
+
+@Composable
+fun HomeScreen(
+    marsUiState: String, modifier: Modifier = Modifier
+) {
+    ResultScreen(marsUiState, modifier)
+}
+
+/**
+ * ResultScreen displaying number of photos retrieved.
+ */
+@Composable
+fun ResultScreen(photos: String, modifier: Modifier = Modifier) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier
+    ) {
+        Text(text = photos)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ResultScreenPreview() {
+    MarsPhotosTheme {
+        ResultScreen(stringResource(R.string.placeholder_result))
+    }
+}
